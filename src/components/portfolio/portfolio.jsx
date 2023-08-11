5a04e05a66374f35bb4c2b85765ec144d03528bf
Subhangam Dey <deysubhangam@gmail.com>
2023-08-11 13:30:31 +0530

    made changes in .giitgnore coz git push isn't working

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.png'
-import IMG2 from '../../assets/portfolio2.jpg'
-import IMG3 from '../../assets/portfolio3.jpg'
-import IMG4 from '../../assets/portfolio4.jpg'
-import IMG5 from '../../assets/portfolio5.jpg'
-import IMG6 from '../../assets/portfolio6.jpg'
+import IMG2 from '../../assets/portfolio2.png'
+import IMG3 from '../../assets/portfolio3.png'
+import IMG4 from '../../assets/portfolio4.png'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.png'
 const data=[
     {
         id : 1,
@@ -41,7 +41,7 @@ const data=[
         img : IMG5,
         title : "ReactJS major project",
         liveDemo : "https://my-app-subhangamdey.vercel.app/",
-        github : "https://github.com/subhangamdey/Drum-Kit"
+        github : "https://github.com/subhangamdey/React-portfolio-testing"
     },
     {
         id : 6,
